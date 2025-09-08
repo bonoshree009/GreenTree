f290f5dbfdfb42f28f451cc4c8bd2c8980788307
bonoshree009 <bonoshree.sarkarbd@gmail.com>
2025-09-09 00:36:42 +0600

    all modals are done

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ let cartload =(plants)=>{
     const allcartId = document.getElementById("cart")
    plants.forEach( plant=> {
     const div = document.createElement("div")
-    div.innerHTML = `<div class="p-5 bg-white rounded space-y-3 w-full h-full shadow-lg "> <img src="${plant.image}" alt="" class="object-cover w-full h-40">
-                        <h1 class="font-bold ">${plant.name}</h1>
+    div.innerHTML = `<div class="p-2 bg-white rounded space-y-3 w-full h-full shadow-lg "> <img src="${plant.image}" alt="" class="object-cover w-full h-40">
+                        <h1 class="font-bold" onclick="loadtreeinfo(${plant.id})">${plant.name}</h1>
                         <p class=" text-gray-600 ">${plant.description}</p>
                       <div class="flex justify-between">
                         <button class="btn rounded-3xl bg-[#f0fdf4] text-[#15803d] border-0">${plant.category}</button>
@@ -55,11 +55,30 @@ const hover =(id)=> {
              });
              document.getElementById(id).classList.add("buttonHober")
         let categoryName = document.getElementById(id).innerText;
+
        document.getElementById("cart").innerHTML = "";
 
         let filtered = allplants.filter(filter =>filter.category === categoryName)
         cartload(filtered)
              
+}
+const loadtreeinfo= (id)=>{
+      const plant = allplants.find( p => p.id == id)
+   const modal=document.getElementById("modal-container")
+   modal.innerHTML =`<div class="modal-box space-y-2">
+    <h3 class="text-lg font-bold">${plant.name}</h3>
+    <img src="${plant.image}" alt="" class="object-cover w-full h-40">
+    <P> <span  class="font-bold">categries:</span>${plant.category}</p>
+     <P> <span  class="font-bold">price:</span>৳${plant.price}</p>
+      <P><span  class="font-bold">Description:</span>${plant.description}</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>`
+  my_modal_1.showModal()
+  
 }
 
 categoriesfetch()
